29372d542cf04823902f8ebd08a1f1c88d3ba138
kazuhi-ra <kazuhira4126@gmail.com>
2020-07-22 03:34:49 +0900

    コールバック地獄解消のリファクタリング

--- ch03/index.js
+++ ch03/index.js
@@ -4,34 +4,36 @@ const mkdirp = require('mkdirp')
 const path = require('path')
 const utilities = require('./utilities')
 
+const saveFile = (filename, contents, callback) => {
+  mkdirp(path.dirname(filename), err => {
+    if (err) {
+      return callback(err)
+    }
+    fs.writeFile(filename, contents, callback)
+  })
+}
+
+const download = (url, filename, callback) => {
+  console.log(`Downloading ${url}`)
+  request(url, (err, response, body) => {
+    if (err) return callback(err)
+
+    saveFile(filename, body, err => {
+      if (err) return callback(err)
+      callback(null, body)
+    })
+  })
+}
+
 // 指定したurlのHTMLを保存し、終わったらcallbackを呼び出す
 const spider = (url, callback) => {
   const filename = utilities.urlToFilename(url)
   fs.exists(filename, exists => {
-    if (!exists) {
-      console.log(`Downloading ${url}`)
-      request(url, (err, response, body) => {
-        if (err) {
-          callback(err)
-        } else {
-          mkdirp(path.dirname(filename), err => {
-            if (err) {
-              callback(err)
-            } else {
-              fs.writeFile(filename, body, err => {
-                if (err) {
-                  callback(err)
-                } else {
-                  callback(null, filename, true)
-                }
-              })
-            }
-          })
-        }
-      })
-    } else {
-      callback(null, filename, false)
-    }
+    if (exists) return callback(null, filename, false)
+    download(url, filename, err => {
+      if (err) return callback(err)
+      callback(null, filename, true)
+    })
   })
 }
 
